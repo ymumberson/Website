72ed2f2e419f6c802fe44c4991165d701a4ec8eb
Yoshan Mumberson <mryosh@outlook.com>
2023-07-07 16:50:44 +0100

    Implemented Overview and About section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,11 +19,7 @@ const Navbar = () => {
             }}
           >
             <img src={logo} alt="logo" className="w-9 h-9 object-contain"/>
-          <p className='text-white text-[18px] font-bold cursor-pointer'>
-            Yoshan Mumberson 
-              <span className='sm:block hidden'>
-                | Software Developer
-              </span>
+          <p className='text-white text-[18px] font-bold cursor-pointer flex'>Yoshan &nbsp; <span className='sm:block hidden'>| Software Developer</span>
           </p>
           </Link>
           <ul className="list-none hidden sm:flex flex-row gap-10">
