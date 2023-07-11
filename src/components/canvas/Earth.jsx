4f78a8e5015c734ddcb955357d08ccd2fdd43d98
Yoshan Mumberson <mryosh@outlook.com>
2023-07-11 17:01:07 +0100

    Changed planet model

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -4,16 +4,37 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from '../Loader';
 
 const Earth = () => {
-  const earth = useGLTF('./planet/scene.gltf')
-
+  const earth = useGLTF('./low_poly_planet_earth/scene.gltf')
   return (
+  <mesh>
+    <hemisphereLight intensity={0.15} groundColor="black" />
+      <pointLight intensity={1} />
+      <spotLight 
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
     <primitive
       object={earth.scene}
-      scale={2.5}
-      position-y={0}
+      scale={1.5}
+      position-y={-2}
       rotation-y={0}
     />
+    </mesh>
   )
+
+  // const earth = useGLTF('./planet/scene.gltf')
+  // return (
+  //   <primitive
+  //     object={earth.scene}
+  //     scale={2.5}
+  //     position-y={0}
+  //     rotation-y={0}
+  //   />
+  // )
 }
 
 const EarthCanvas = () => {
