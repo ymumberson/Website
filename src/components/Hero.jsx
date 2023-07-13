7a4606a2fc05c8c5969ccb85a830f8b2406110b3
Yoshan Mumberson <mryosh@outlook.com>
2023-07-13 12:06:25 +0100

    Changed colour of name and gradient line

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,16 +9,13 @@ const Hero = () => {
     <section className='relative w-full h-screen mx-auto'>
       <div className='${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5'>
         <div className='flex flex-col justify-center items-center mt-5'>
-          <div className='w-5 h-5 rounded-full bg-[#96CCFF]'/>
+          <div className='w-5 h-5 rounded-full bg-[#00ddff]'/>
           <div className='w-1 sm:h-80 h-40 blue-gradient'/>
         </div>
 
         <div>
-          {/* <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#96CCFF]'>Yoshan</span>
-          </h1> */}
           <h1 className={`${styles.heroHeadText} text-white`}>
-            {titles.find(({ id }) => id === "Title").text}<span className='text-[#96CCFF]'>{strings.find(({ id }) => id === "Name").text}</span>
+            {titles.find(({ id }) => id === "Title").text}<span className='text-[#00ddff]'>{strings.find(({ id }) => id === "Name").text}</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
           {strings.find(({ id }) => id === "Title-text").text}
